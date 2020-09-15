13406071c40cc14c3448e705217254e4640ed93b
MOJEED KUSIMO <mkusimo90@gmail.com>
2020-09-15 01:46:45 +0100

    Updates

---
+++ precache-manifest.ccdc20d7d7d717487b52cdb1c2c8e7a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a44e29141b1fe10c0f54b70daa31dc",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "15649e978f92c86f2323",
+    "url": "/todoapp/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ed6f3358b9fb36e7d49f",
+    "url": "/todoapp/static/css/main.94cf8c89.chunk.css"
+  },
+  {
+    "revision": "15649e978f92c86f2323",
+    "url": "/todoapp/static/js/2.1a809a83.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todoapp/static/js/2.1a809a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed6f3358b9fb36e7d49f",
+    "url": "/todoapp/static/js/main.a483fa49.chunk.js"
+  },
+  {
+    "revision": "afdd9e638a87acd072ad",
+    "url": "/todoapp/static/js/runtime-main.fbd16662.js"
+  }
+]);
